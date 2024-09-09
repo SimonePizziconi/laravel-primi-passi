696b865b4629eea31558987cc625e654812a477c
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-09 13:32:56 +0200

    feat: print 'Hello World' in home page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // Hello World
+
+    $title = 'Hello World';
+
+    return view('home', compact('title'));
 });
